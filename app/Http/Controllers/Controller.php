598a3443de1756a9d60f5b0cee2a561cbf861525
ANDRIANANTENAINA001Angelo <morningstar25angelo@gmail.com>
2024-08-23 16:11:31 +0300

    swagger config

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API Documentation",
+ *      description="API documentation for the Ticket Place App",
+ *      @OA\Contact(
+ *          email="ticket.place@gmail.com"
+ *      ),
+ *      @OA\License(
+ *          name="Apache 2.0",
+ *          url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *      )
+ * )
+ */
+
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
